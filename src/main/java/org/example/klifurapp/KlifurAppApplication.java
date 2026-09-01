bea13ee4ba50dfe66e3dd916152872b576783f48
Runar Thor <rthor997@gmail.com>
2026-09-01 13:19:33 +0000

    Java Spring template

---
+++ src/main/java/org/example/klifurapp/KlifurAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.klifurapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlifurAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlifurAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/klifurapp/KlifurAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.klifurapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlifurAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
