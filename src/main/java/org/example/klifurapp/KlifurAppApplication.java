b893ec6ca3409170a2014e87570d21cd2189a362
Dagur Ingi <daguringividar@gmail.com>
2026-09-14 12:34:44 +0000

    Removed team hellos

--- src/main/java/org/example/klifurapp/KlifurAppApplication.java
+++ src/main/java/org/example/klifurapp/KlifurAppApplication.java
@@ -3,15 +3,12 @@ package org.example.klifurapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Yeeaaahhhhh babyyyyyyy
 
 @SpringBootApplication
 public class KlifurAppApplication {
 
     public static void main(String[] args) {
-        System.out.println("Halló halló");
         SpringApplication.run(KlifurAppApplication.class, args);
-        System.out.println("Bæ bæ");
     }
 
 }
