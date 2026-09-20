965e537a740e8f49e22ed930b0a9ab07011982eb
Runar Thor <rthor997@gmail.com>
2026-09-20 17:20:14 +0000

    Klára UC2 UserController, SecurityConfig og uppfæra AuthServiceImplementation til að hafa passwordencoder.

--- src/main/java/org/example/klifurapp/Controller/UserController.java
+++ src/main/java/org/example/klifurapp/Controller/UserController.java
@@ -1,4 +1,25 @@
 package org.example.klifurapp.Controller;
 
+import jakarta.validation.Valid;
+import org.example.klifurapp.DTO.user.CreateUserDTO;
+import org.example.klifurapp.entity.User;
+import org.example.klifurapp.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final AuthService authService;
+
+    public UserController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody CreateUserDTO dto) {
+        return authService.register(dto);
+    }
 }
