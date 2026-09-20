ec01e4dd869ef605b7960aabfd88d96918f7b56a
Dagur Ingi <daguringividar@gmail.com>
2026-09-20 23:29:02 +0000

    Login Controller complete

--- src/main/java/org/example/klifurapp/Controller/UserController.java
+++ src/main/java/org/example/klifurapp/Controller/UserController.java
@@ -1,5 +1,8 @@
 package org.example.klifurapp.Controller;
 
+import org.example.klifurapp.DTO.user.LoginRequestDTO;
+import org.example.klifurapp.DTO.user.LoginResponseDTO;
+import org.example.klifurapp.service.AuthService;
 import org.example.klifurapp.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,9 +12,12 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final AuthService authenticatorService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService,
+                          AuthService authenticatorService) {
         this.userService = userService;
+        this.authenticatorService = authenticatorService;
     }
 
     @DeleteMapping("/{id}")
@@ -19,5 +25,14 @@ public class UserController {
         userService.deleteUser(id);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(
+            @RequestBody LoginRequestDTO dto) {
+
+        LoginResponseDTO responseDto = authenticatorService.authenticate(dto);
+
+        return ResponseEntity.ok(responseDto);
     }
 }
