b717e222ef0ded3ed94942a5f5b34d8054676ad1
Dagur Ingi <daguringividar@gmail.com>
2026-09-20 23:07:30 +0000

    Login user fix

--- src/main/java/org/example/klifurapp/repository/UserRepository.java
+++ src/main/java/org/example/klifurapp/repository/UserRepository.java
@@ -11,5 +11,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
-    Optional<User> findByEmailAndPasswordHash(String email, String password);
 }
